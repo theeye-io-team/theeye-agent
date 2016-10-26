4479df81a3c3a841e1056d1cf213dd892ba43a2f
Facugon <facugon@interactar.com>
2016-10-26 12:38:36 -0300

    optional timeout in options

--- core/lib/theeye-client/index.js
+++ core/lib/theeye-client/index.js
@@ -66,7 +66,7 @@ TheEyeClient.prototype = {
     connection.request = request.defaults({
       proxy: process.env.http_proxy,
       tunnel: false,
-      timeout: 5000,
+      timeout: (options.timeout||5000),
       json: true,
       gzip: true,
       headers: {
