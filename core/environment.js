9e22cf799f5a8197b3250412ff094ac956eba14e
Facundo Gonzalez <facugon@interactar.com>
2016-11-24 16:37:13 -0300

    remove __dirname

--- core/environment.js
+++ core/environment.js
@@ -4,12 +4,14 @@ module.exports = function (next) {
   var env = process.env.NODE_ENV ;
   var scriptsPath = process.env.THEEYE_AGENT_SCRIPT_PATH ;
 
-  if( ! scriptsPath ) scriptsPath = __dirname + '/../scripts' ;
+  if (!scriptsPath) {
+    scriptsPath = process.cwd() + '/../scripts' ;
+  }
 
   process.env.THEEYE_AGENT_SCRIPT_PATH = scriptsPath;
 
   fs.exists(scriptsPath, function(exists){
-    if( ! exists ) {
+    if (!exists) {
       fs.mkdirSync(scriptsPath,'0755');
     }
     next(scriptsPath);
