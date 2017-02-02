ba8401d7d0cc92e98f58ee01ca678761e4bcf8b0
Facugon <facugon@interactar.com>
2017-02-02 10:12:35 -0300

    add uid and gid validation (set default if none)

--- core/lib/file/index.js
+++ core/lib/file/index.js
@@ -6,28 +6,90 @@ var md5 = require('md5');
 var fs = require('fs');
 var util = require('util');
 var mkdirp = require('mkdirp');
+//var uidNumber = require('uid-number');
 
 var debug = require('debug')('eye:lib:file');
 
 /**
  * convert the returned stat.mode to the unix octal form
+ * @return string
  */
 function statModeToOctalString (mode) {
   var permString = '0' + (mode & parseInt('0777',8)).toString(8);
   return permString;
 }
 
+/**
+ *
+ * convert argument to unix id
+ *
+ * @author Facugon
+ * @param Mixed id
+ * @return {Integer|null}
+ * @private
+ *
+ */
+function parseUnixId (id) {
+  var _id = parseInt(id);
+  if (!Number.isInteger(_id) || id < 0) return null;
+  return id;
+}
+
 function File (props) {
 
   EventEmitter.call(this);
 
-  var _id = props.id;
-  var _md5 = props.md5;
-  var _filename = props.filename;
-  var _path = props.path;
-  var _mode = props.mode;
-  var _uid = parseInt(props.uid);
-  var _gid = parseInt(props.gid);
+  /**
+   * @name _id
+   * @type string
+   * @private
+   */
+  var _id;
+  /**
+   * @name _md5
+   * @type string
+   * @private
+   */
+  var _md5;
+  /**
+   * @name _filename
+   * @type string
+   * @private
+   */
+  var _filename;
+  /**
+   * @name _path
+   * @type string
+   * @private
+   */
+  var _path;
+  /**
+   * @name _mode
+   * @type string string of permissions in the unix octal format
+   * @private
+   */
+  var _mode;
+  /**
+   * @name _uid
+   * @type integer unsigned integer
+   * @private
+   */
+  var _uid;
+  /**
+   * @name _gid
+   * @type integer unsigned integer
+   * @private
+   */
+  var _gid;
+
+  _id = props.id;
+  _md5 = props.md5;
+  _filename = props.filename;
+  _path = props.path;
+  _mode = props.mode; // assuming this is a correct value
+
+  _uid = parseUnixId(props.uid)||process.getuid();
+  _gid = parseUnixId(props.gid)||process.getgid();
 
   if (!_path) throw new Error('path is required.');
   if (!_filename) throw new Error('filename is required.');
@@ -37,37 +99,43 @@ function File (props) {
     get: function() { return _id; },
     enumerable:true,
   });
+
   Object.defineProperty(this,'gid',{
     get: function() { return _gid; },
     enumerable:true,
   });
+
   Object.defineProperty(this,'uid',{
     get: function() { return _uid; },
     enumerable:true,
   });
+
   Object.defineProperty(this,'mode',{
     get: function() { return _mode; },
     enumerable:true,
   });
+
   Object.defineProperty(this,'md5',{
     get: function() { return _md5; },
     enumerable:true,
   });
+
   Object.defineProperty(this,'filepath',{
     get: function() { return _filepath; },
     enumerable:true,
   });
+
   Object.defineProperty(this,'filename',{
     get: function() { return _filename; },
     enumerable:true,
   });
+
   Object.defineProperty(this,'path',{
     get: function() { return _path; },
     enumerable:true,
   });
 
   this.access = function (next) {
-
     debug('checking file access %s', this.filepath);
 
     var accessMode = fs.constants.R_OK | fs.constants.W_OK | fs.constants.X_OK | fs.constants.F_OK ;
@@ -101,13 +169,13 @@ function File (props) {
       }
 
       if (stat.uid !== _uid) {
-        var err = new Error('EOWNER: current file mode is incorrect');
+        var err = new Error('EOWNER: current file uid is incorrect');
         err.code = 'EOWNER';
         return next(err);
       }
 
       if (stat.gid !== _gid) {
-        var err = new Error('EGROUP: current file mode is incorrect');
+        var err = new Error('EGROUP: current file gid is incorrect');
         err.code = 'EGROUP';
         return next(err);
       }
@@ -147,14 +215,22 @@ function File (props) {
 
     debug('setting [mode:%s][uid:%s][gid:%s]',mode,uid,gid);
 
+    if (!mode||isNaN(mode)) mode = parseInt('0755',8);
     fs.chmod(path,mode,function(err){
       if (err) {
         debug(err);
         return next(err);
       }
+
+      debug('mode set');
+
       fs.chown(path,uid,gid,function(err){
-        debug(err);
-        return next(err);
+        if (err) {
+          debug(err);
+          return next(err);
+        }
+        debug('permissions set');
+        return next();
       });
     });
   }
