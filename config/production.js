cd337e760ae4f9ef9662fc0c3d588bd6b6098ead
Facugon <facugon@interactar.com>
2017-02-01 21:25:44 -0300

    roll back production config

--- config/production.js
+++ config/production.js
@@ -1,8 +1,1 @@
-module.exports = {
-  supervisor: {
-    api_url:'https://supervisor.theeye.io',
-    client_id:'theeye',
-    client_secret:'esto es secreto',
-    client_customer:'theeye'
-  }
-}
+module.exports = { }
