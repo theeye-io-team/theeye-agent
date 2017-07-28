916ebcc5d1e915032c6f6a78ef24c6ee4c1ab57b
Facundo Gonzalez <facugon@interactar.com>
2017-07-28 11:52:09 -0300

    update compilation and bundle scripts

---
+++ misc/bundle.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+root="$PWD"
+target="theeye-agent/"
+bin="bin/"
+
+if [ ! -d "$target" ]; then
+  mkdir $target
+fi
+
+echo "copying files"
+
+if [ ! -d "$bin" ]; then
+  echo "$PWD/$bin not found"
+  exit 1
+fi
+
+if [ ! -d "misc" ]; then
+  echo "$PWD/misc not found"
+  exit 1
+fi
+
+if [ ! -f "runBinary.sh" ]; then
+  echo "$PWD/runBinary.sh not found"
+  exit 1
+fi
+
+cp -r $bin $target
+cp -r misc $target
+cp -r runBinary.sh $target
+
+echo "adding version to package"
+
+git describe > $target/version
+
+echo "creating package"
+
+tar -czf theeye-agent64.tar.gz $target
+
+echo "removing temporal files"
+
+rm -rf $target
+
+echo "success"
--- misc/compile.sh
+++ misc/compile.sh
@@ -1,12 +1,79 @@
 #!/bin/bash
 
-npm install npm3
-./node_modules/.bin/npm3 install
+#exec 3>&1 4>&2
+#trap 'exec 2>&4 1>&3' 0 1 2 3
+#exec 1>run_enclose.out 2>&1
+# Everything below will go to the file 'run_enclose.out':
 
-AGENT_DIR=`pwd`
-#npm install
-cd node_modules/config
-npm install hjson toml cson properties
-cd $AGENT_DIR
+root="$PWD"
+target="bin"
+node_version="node6-linux"
+release="$target/release"
 
-node_modules/.bin/enclose --features=no -l warning -o core/bin.exe -v v4.4.7 core/main.js 
+if [ -d "$target" ]; then
+  rm -rf $target
+fi
+
+if [ -d "node_modules" ]; then
+  echo "rebuilding node_modules"
+  rm -rf "$PWD/node_modules"
+fi
+
+command -v npm >/dev/null 2>&1 || { echo >&2 "npm is required but it's not installed."; echo "failure"; exit 1; }
+
+npm install --production
+
+echo "creating $target"
+mkdir $target
+
+if [ ! -d "node_modules" ]; then
+  echo "running npm --production"
+  npm install
+fi
+
+#cd $root/node_modules/config
+
+echo "running npm version $(npm --version)"
+
+# add extra dependencies - not used by the agent anyway. but just to avoid errors and warnings
+npm install hjson toml cson properties x2js
+
+# run enclose
+cd $root
+
+command -v pkg >/dev/null 2>&1 || { echo >&2 "pkg is required but it's not installed."; echo "failure"; exit 2; }
+
+pkg -v 
+echo "running pkg"
+pkg --targets $node_version --output $target/theeye-agent --debug core/main.js
+
+# copy bindings
+echo "copying bindings"
+cp node_modules/userid/build/Release/userid.node $target
+cp node_modules/ref/build/Release/binding.node $target
+cp node_modules/ffi/build/Release/ffi_bindings.node $target
+
+echo "copying configs"
+# copy configs
+if [ ! -d "$target/config" ]; then
+  echo "creating $target/config"
+  mkdir $target/config
+fi
+cp config/default.js $target/config
+
+if [ -z "$NODE_ENV" ]; then
+  NODE_ENV=''
+fi
+
+echo "copying $NODE_ENV config"
+cp config/$NODE_ENV.js $target/config
+
+echo -e "Agent Version \t\t$(git describe)" >> $release
+echo -e "NODE_ENV config \t\t$NODE_ENV" >> $release
+echo -e "Node Version \t\t$node_version" >> $release
+echo -e "NPM Version \t\t$(npm --version)" >> $release
+
+echo "summary"
+cat $target/release
+
+echo "success"
