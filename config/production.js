db2fa9137b6df39851f9c2234d72b76fe40e8e60
Facundo <facugon@theeye.io>
2021-01-05 11:38:58 -0300

    Update production.js

--- config/production.js
+++ config/production.js
@@ -4,9 +4,9 @@ module.exports = {
   },
     "supervisor": {
       "api_url": "https://supervisor.theeye.io",
-      "client_id": "b8f945743826bc430e354ada758768e4d60e764f",
-      "client_secret": "d1f1b76961e262cd2362ad6c99e23514cadb2b8c",
-      "client_customer": "FacugonSG",
-      "client_hostname": "WindowsVM"
+      "client_id": "",
+      "client_secret": "",
+      "client_customer": "",
+      "client_hostname": ""
     }
 }
