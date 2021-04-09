23641b507d9e44caddc2a9858e6b880c8a95af3a
facugon <facugon@theeye.io>
2021-04-09 16:57:57 -0300

    improve dockerRun script

--- misc/dockerRun.sh
+++ misc/dockerRun.sh
@@ -2,17 +2,48 @@ clientId=${THEEYE_CLIENT_ID}
 clientSecret=${THEEYE_CLIENT_SECRET}
 clientCustomer=${THEEYE_CLIENT_CUSTOMER}
 clientHostname=${THEEYE_CLIENT_HOSTNAME}
-apiUrl="https://supervisor.theeye.io"
 debug="*eye*"
 name="${clientCustomer}"
-image="${1}"
+
+for i in "$@"
+do
+	case $i in
+		-u=*|--url=*)
+			API_URL="${i#*=}"
+			shift # past argument=value
+			;;
+		-i=*|--image=*)
+			IMAGE="${i#*=}"
+			shift # past argument=value
+			;;
+		--default)
+			DEFAULT=YES
+			shift # past argument with no value
+			;;
+		*)
+			# unknown option
+			;;
+	esac
+done
+
+if [ -z ${IMAGE+x} ];
+then
+	echo "docker image required. use: -i|--image="
+	exit
+fi
+
+if [ -z ${API_URL+x} ];
+then
+	echo "supervisor url required. use: -u|--url="
+	exit
+fi
 
 docker run  --memory=512m -e NODE_ENV='production' \
   -e THEEYE_SUPERVISOR_CLIENT_ID="${clientId}" \
   -e THEEYE_SUPERVISOR_CLIENT_SECRET="${clientSecret}"  \
   -e THEEYE_SUPERVISOR_CLIENT_CUSTOMER="${clientCustomer}"  \
   -e THEEYE_CLIENT_HOSTNAME="${clientHostname}" \
-  -e THEEYE_SUPERVISOR_API_URL="${apiUrl}" \
+  -e THEEYE_SUPERVISOR_API_URL="${API_URL}" \
   -e DEBUG="${debug}" \
   -v /home/facugon/workspace/theeye/theeye-mailbot:/opt/theeye/theeye-mailbot \
-  --name "${name}" --restart always -d "${image}"
+  --name "${name}" --restart always -d "${IMAGE}"
